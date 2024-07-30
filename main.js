4e0bf77f9a8753f406f9b7f16ebea966118a7938
anuselva1905 <selvasekar28655@gmail.com>
2024-07-30 10:28:24 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+// const apiKey = "a1de372ff85bff9ff1105e4c85f4c7a3";
+// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=matric&q=";
+// const searchBox = document.querySelector(".search input");
+// const searchBtn = document.querySelector(".search button");
+
+
+
+// async function checkWeather(city){
+//     const response = await fetch(apiUrl+ city + `&appid=${apiKey}`);
+//     var data = await response.json();
+
+//     console.log(data);
+
+//     document.querySelector(".city") .innerHTML = data.name;
+//     document.querySelector(".temp") .innerHTML = math.round(data.main.temp) +"°C";
+//     document.querySelector(".humidity") .innerHTML = data.main.humidity + "%";
+//     document.querySelector(".wind") .innerHTML = data.wind.speed + "km\h";
+// }
+
+// searchBtn.  addEventListener("click", ()=>{
+//     checkWeather(searchBox.value);
+// })
+
+
+const apiKey = "a1de372ff85bff9ff1105e4c85f4c7a3";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
